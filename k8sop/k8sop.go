74cbf565983c2df96031220fc3bf1efad33f2b84
bcgodev <21715194+bcgodev@users.noreply.github.com>
2021-05-31 13:43:42 +0800

    feat(k8sop): add the ability to get deployment tag in the latest helm release
    add dependencies of helm, kubernetes, and gcp auth

---
+++ k8sop/k8sop.go
@@ -0,0 +1,80 @@
+package k8sop
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/kube"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+)
+
+const (
+	DeploymentTagKey = "tag"
+	kubeConfigPath   = "/dummypath"
+)
+
+func getHelmReleaseManifest(name string, namespace string) (string, error) {
+
+	actionConfig := new(action.Configuration)
+
+	// You can pass an empty string instead of settings.Namespace() to list
+	// all namespaces
+	if err := actionConfig.Init(kube.GetConfig(kubeConfigPath, "", namespace), namespace, os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
+		log.Printf("%+v", err)
+		return "", err
+	}
+
+	get := action.NewGet(actionConfig)
+
+	get.Version = 0
+	release, err := get.Run(name)
+	if err != nil {
+		return "", err
+	}
+	return release.Manifest, nil
+}
+
+func GetHelmReleaseImageTag(name string) (map[string]string, error) {
+
+	manifest, err := getHelmReleaseManifest(name, "default")
+	if err != nil {
+		return nil, err
+	}
+
+	type Deployment struct {
+		Spec struct {
+			Template struct {
+				Spec struct {
+					Containers []struct {
+						Image string `yaml:"image"`
+					} `yaml:"containers"`
+				} `yaml:"spec"`
+			} `yaml:"template"`
+		} `yaml:"spec"`
+	}
+
+	resources := strings.Split(manifest, "---\n")
+	var d Deployment
+	for _, resource := range resources {
+		if strings.Contains(resource, "kind: Deployment") {
+			fmt.Println(resource)
+			err = yaml.Unmarshal([]byte(resource), &d)
+			if err != nil {
+				return nil, err
+			}
+			break
+		}
+	}
+
+	imageParts := strings.Split(d.Spec.Template.Spec.Containers[0].Image, ":")
+	tag := imageParts[len(imageParts)-1]
+
+	return map[string]string{
+		DeploymentTagKey: tag,
+	}, nil
+}
---
+++ k8sop/k8sop_test.go
@@ -0,0 +1,42 @@
+package k8sop
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestListHelmReleaseTV(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[string]string
+		wantErr bool
+	}{
+		{
+			name: "www release",
+			args: args{
+				name: "www",
+			},
+			want: map[string]string{
+				// FIXME it's a temp test case
+				"tag": "master__272",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ListHelmReleaseTV(tt.args.name)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ListHelmReleaseTV() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ListHelmReleaseTV() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

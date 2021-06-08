9c94cec6fdc9e8d5297a15b50577f688ee2d5f84
bcgodev <21715194+bcgodev@users.noreply.github.com>
2021-06-08 21:53:57 +0800

    test(gitop): update the gitop_test according to the new Repository fields

--- internal/gitop/gitop_test.go
+++ internal/gitop/gitop_test.go
@@ -1,25 +1,24 @@
 package gitop
 
 import (
-	"fmt"
-	"reflect"
 	"sync"
 	"testing"
 
-	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 )
 
 func TestRepository_GetFile(t *testing.T) {
 	repo, err := GetRepository("tv")
 	if err != nil {
-		fmt.Println(err)
-		panic(1)
+		t.Error(err)
 	}
 	type fields struct {
-		config map[string]string
-		once   *sync.Once
-		r      *git.Repository
+		authMethod ssh.AuthMethod
+		config     map[string]string
+		once       *sync.Once
+		r          *git.Repository
+		locker     *sync.Mutex
 	}
 	type args struct {
 		filenamePath string
@@ -28,35 +27,36 @@ func TestRepository_GetFile(t *testing.T) {
 		name    string
 		fields  fields
 		args    args
-		want    billy.File
 		wantErr bool
 	}{
 		{
 			name: "dev for tv",
 			fields: fields{
-				config: gitConfig["tv"],
-				once:   repo.once,
-				r:      repo.r,
+				authMethod: repo.authMethod,
+				config:     gitConfig["tv"],
+				once:       repo.once,
+				r:          repo.r,
+				locker:     repo.locker,
 			},
 			args: args{
 				filenamePath: "cms/values-prod.yaml",
 			},
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			repo := &Repository{
-				config: tt.fields.config,
-				once:   tt.fields.once,
-				r:      tt.fields.r,
+				authMethod: tt.fields.authMethod,
+				config:     tt.fields.config,
+				once:       tt.fields.once,
+				r:          tt.fields.r,
+				locker:     tt.fields.locker,
 			}
-			got, err := repo.GetFile(tt.args.filenamePath)
+			_, err := repo.GetFile(tt.args.filenamePath)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Repository.GetFile() error = %v, wantErr %v", err, tt.wantErr)
 				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Repository.GetFile() = %v, want %v", got, tt.want)
 			}
 		})
 	}

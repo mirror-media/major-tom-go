943ae31d4be84f954a965aaa0363b3440bbfd810
bcgodev <21715194+bcgodev@users.noreply.github.com>
2021-06-09 03:05:53 +0800

    feat(command): add deploy functionality

--- internal/command/command.go
+++ internal/command/command.go
@@ -1,2 +1,8 @@
 // Package command implement the operation controller
 package command
+
+// CommandResponse is supposed to use in a channel to pass response for different go routine
+type CommandResponse struct {
+	Messages []string
+	Error    error
+}

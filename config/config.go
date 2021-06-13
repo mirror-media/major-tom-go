4c403b7ed79f3eea96fd67ee19846aa0c894e7da
Baron Chiu <21715194+bcgodev@users.noreply.github.com>
2021-06-13 19:30:19 +0800

    Refactor/git config (#9)
    * feat(config): add GitConfigs
    * feat(gitop): add known_hosts to sshMethod
    
    Refs: #9

--- config/config.go
+++ config/config.go
@@ -5,8 +5,18 @@ type Stage string
 type Project string
 type K8S map[Project]map[Stage]KubeConfigPath
 
+type Repository string
+type GitConfig struct {
+	Branch        string `yaml:"branch"`
+	SSHKeyPath    string `yaml:"sshKeyPath"`
+	SSHKeyUser    string `yaml:"sshKeyUser"`
+	SSHKnownhosts string `yaml:"sshKnownhosts"`
+	URL           string `yaml:"url"`
+}
+
 type Config struct {
-	SlackBotToken  string `yaml:"slackBotToken"`
-	SlackToken     string `yaml:"slackToken"`
-	ClusterConfigs K8S    `yaml:"clusterConfigs"`
+	SlackBotToken  string                   `yaml:"slackBotToken"`
+	SlackToken     string                   `yaml:"slackToken"`
+	ClusterConfigs K8S                      `yaml:"clusterConfigs"`
+	GitConfigs     map[Repository]GitConfig `yaml:"gitConfigs"`
 }

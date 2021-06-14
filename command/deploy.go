mirror-media/major-tom-go

commit 601b4a9304246e242fa3f81579ff61cdd971ed94

Bug 12 (#13)
* fix(deploy): dump the modified values yaml with gookit/config/v2
* feat(bod)!: use gookit to load config with a flag "-c"
* refactor(config)!: rename SlackToken to SlackAppToken
* refactor(ci): use ENTRYPOINT instead of CMD to leverage k8s arguments in deployement

Refs: 12

command/deploy.go
@@ -3,6 +3,7 @@ package command
 import (
 	"context"
 	"fmt"
+	"io"
 	"sort"
 	"strconv"
 	"strings"
@@ -10,13 +11,13 @@ import (
 	"time"
 
 	"github.com/go-git/go-git/v5/plumbing"
+	gootkitconfig "github.com/gookit/config/v2"
+	"github.com/gookit/config/v2/yaml"
 	"github.com/mirror-media/major-tom-go/config"
 	"github.com/mirror-media/major-tom-go/gitop"
 	mjcontext "github.com/mirror-media/major-tom-go/internal/context"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
-	"gopkg.in/yaml.v2"
 )
 
 type yamlCFG struct {
@@ -157,11 +158,9 @@ func deploy(ctx context.Context, clusterConfigs config.K8S, gitConfigs map[confi
 		}
 
 		// operation starts here. worktree needs to be cleaned if disaster happens
-		viper := viper.New()
-		viper.SetConfigType("yaml")
-		err = viper.ReadConfig(f)
+		b, err := io.ReadAll(f)
 		if err != nil {
-			err = errors.Wrap(err, fmt.Sprintf("parsing %s has error", valuesFilePath))
+			err = errors.Wrap(err, fmt.Sprintf("reading %s has error", valuesFilePath))
 			logrus.Warn(err)
 			ch <- response{
 				Messages: messages,
@@ -170,16 +169,15 @@ func deploy(ctx context.Context, clusterConfigs config.K8S, gitConfigs map[confi
 			_ = hardResetFn()
 			return
 		}
-		for key, value := range operations {
-			viper.Set(key, value)
-		}
-		c := viper.AllSettings()
-
-		b, err := yaml.Marshal(c)
+		valueConfig := gootkitconfig.New(valuesFilePath)
+		valueConfig.AddDriver(yaml.Driver)
+		err = valueConfig.LoadStrings(gootkitconfig.Yaml, string(b))
 		if err != nil {
+			err = errors.Wrap(err, fmt.Sprintf("loading YAML from %s has error", valuesFilePath))
+			logrus.Warn(err)
 			ch <- response{
 				Messages: messages,
-				Error:    errors.Wrap(err, fmt.Sprintf("marshalling %s has error", valuesFilePath)),
+				Error:    err,
 			}
 			_ = hardResetFn()
 			return
@@ -195,15 +193,9 @@ func deploy(ctx context.Context, clusterConfigs config.K8S, gitConfigs map[confi
 			_ = hardResetFn()
 			return
 		}
-		err = func() error {
-			f, err = repo.GetFile(valuesFilePath)
-			if err != nil {
-				return err
-			}
-			_, err = f.Write(b)
-			f.Close()
-			return err
-		}()
+
+		f, _ = repo.GetFile(valuesFilePath)
+		valueConfig.DumpTo(f, gootkitconfig.Yaml)
 		if err != nil {
 			ch <- response{
 				Messages: messages,

build/package/Dockerfile
@@ -14,4 +14,4 @@ WORKDIR /major-tom
 
 COPY --from=build /major-tom/bin/ .
 
-CMD [ "./bot" ]
+ENTRYPOINT [ "sh", "-c", "./bot" ]

cmd/bot/bot.go
@@ -9,15 +9,16 @@ import (
 	"runtime"
 	"strings"
 
-	"github.com/fsnotify/fsnotify"
+	gootkitconfig "github.com/gookit/config/v2"
+	"github.com/gookit/config/v2/yaml"
 	"github.com/mirror-media/major-tom-go/command"
 	"github.com/mirror-media/major-tom-go/config"
 	"github.com/mirror-media/major-tom-go/slashcommand"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
 	"github.com/slack-go/slack/socketmode"
-	"github.com/spf13/viper"
 )
 
 func main() {
@@ -30,28 +31,30 @@ func main() {
 			},
 		})
 
-	viper.SetConfigName("config")    // name of config file (without extension)
-	viper.SetConfigType("yaml")      // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("./configs") // path to look for the config file in
-	err := viper.ReadInConfig()      // Find and read the config file
-	if err != nil {                  // Handle errors reading the config file
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
-	}
-
-	viper.WatchConfig()
-	viper.OnConfigChange(func(e fsnotify.Event) {
-		// FIXME what should I do with changed file?
-		fmt.Println("Config file changed:", e.Name)
-	})
-
 	var cfg config.Config
 
-	err = viper.Unmarshal(&cfg)
+	// parse flag
+	botFlags := gootkitconfig.New("bot config")
+	err := botFlags.LoadFlags([]string{"c:string"})
 	if err != nil {
-		panic(fmt.Errorf("fatal error unmarshalling config file: %s \n", err))
+		panic(errors.Wrap(err, "loading flags for config file has error"))
 	}
 
-	appToken := cfg.SlackToken
+	botConfig := gootkitconfig.New("bot config")
+	botConfig.AddDriver(yaml.Driver)
+
+	// load config file
+	err = botConfig.LoadFiles(botFlags.String("c"))
+	if err != nil {
+		panic(errors.Wrap(err, "loading config file has error"))
+	}
+
+	err = botConfig.BindStruct("", &cfg)
+	if err != nil {
+		panic(fmt.Errorf("fatal error binding config file to struct: %s", err))
+	}
+
+	appToken := cfg.SlackAppToken
 
 	api := slack.New("",
 		slack.OptionDebug(true),

config/config.go
@@ -15,8 +15,8 @@ type GitConfig struct {
 }
 
 type Config struct {
-	SlackBotToken  string                   `yaml:"slackBotToken"`
-	SlackToken     string                   `yaml:"slackToken"`
 	ClusterConfigs K8S                      `yaml:"clusterConfigs"`
 	GitConfigs     map[Repository]GitConfig `yaml:"gitConfigs"`
+	SlackAppToken  string                   `yaml:"slackAppToken"`
+	SlackBotToken  string                   `yaml:"slackBotToken"`
 }

go.mod
@@ -3,13 +3,12 @@ module github.com/mirror-media/major-tom-go
 go 1.16
 
 require (
-	github.com/fsnotify/fsnotify v1.4.9
 	github.com/go-git/go-billy/v5 v5.3.1
 	github.com/go-git/go-git/v5 v5.4.2
+	github.com/gookit/config/v2 v2.0.24
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/slack-go/slack v0.9.1
-	github.com/spf13/viper v1.7.1
 	gopkg.in/yaml.v2 v2.4.0
 	helm.sh/helm/v3 v3.6.0
 	k8s.io/apimachinery v0.21.1

commit d911bc314f66390e740d2d61c8ede83fd7e5ee01

Fix/a regression bug in #13 (#15)
* fix(deploy): add operation to deploy. closes #14
* feat(deploy): add cfg from slashcommand to the message

command/deploy.go
@@ -81,15 +81,20 @@ func (w *deployWorker) Init(gitConfigs map[config.Repository]config.GitConfig) {
 	})
 }
 
+type operation struct {
+	keyPath string
+	value   interface{}
+}
+
 // deploy deploy certain configuration to a service. textParts in interpreted as [project, stage, service, ...cfg:value]
 func deploy(ctx context.Context, clusterConfigs config.K8S, gitConfigs map[config.Repository]config.GitConfig, textParts []string, caller string) {
-	var messages []string
+	var messages = []string{}
 	var err error
 	ch := ctx.Value(mjcontext.ResponseChannel).(chan response)
 	switch len(textParts) {
 	// Deploy needs cfg:arg to operation on
 	case 0, 1, 2, 3:
-		messages = []string{"call help"}
+		messages = append([]string{"call help"}, messages...)
 		err = errors.New("Please provide more details...")
 	// textParts contains the config to deploy
 	default:
@@ -108,7 +113,7 @@ func deploy(ctx context.Context, clusterConfigs config.K8S, gitConfigs map[confi
 		stage := textParts[1]
 		service := textParts[2]
 		args := textParts[3:]
-		operations := map[string]interface{}{}
+		operations := map[string]operation{}
 		// Validate the arguments
 		for _, arg := range args {
 			split := strings.Split(arg, ":")
@@ -125,11 +130,24 @@ func deploy(ctx context.Context, clusterConfigs config.K8S, gitConfigs map[confi
 			var t string
 			switch t = config.valueType; t {
 			case "string":
-				operations[config.path] = value
+				operations[cfg] = operation{
+					keyPath: config.path,
+					value:   value,
+				}
 			case "int":
-				operations[config.path], err = strconv.ParseInt(value, 10, 0)
+				var v int64
+				v, err = strconv.ParseInt(value, 10, 0)
+				operations[cfg] = operation{
+					keyPath: config.path,
+					value:   v,
+				}
 			case "bool":
-				operations[config.path], err = strconv.ParseBool(value)
+				var v bool
+				v, err = strconv.ParseBool(value)
+				operations[cfg] = operation{
+					keyPath: config.path,
+					value:   v,
+				}
 			}
 			if err != nil {
 				err = errors.Wrap(err, fmt.Sprintf("value type of %s is %s, but %s is provided", cfg, config.valueType, value))
@@ -183,6 +201,10 @@ func deploy(ctx context.Context, clusterConfigs config.K8S, gitConfigs map[confi
 			return
 		}
 
+		for _, operation := range operations {
+			valueConfig.Set(operation.keyPath, operation.value, true)
+		}
+
 		err = f.Truncate(0)
 		f.Close()
 		if err != nil {
@@ -218,18 +240,22 @@ func deploy(ctx context.Context, clusterConfigs config.K8S, gitConfigs map[confi
 			return
 		}
 
-		var body string
 		sortedOperations := make([]string, 0, len(operations))
-		for path := range operations {
-			sortedOperations = append(sortedOperations, path)
+		for cfg := range operations {
+			sortedOperations = append(sortedOperations, cfg)
 		}
 		sort.Strings(sortedOperations)
-		for _, path := range sortedOperations {
-			body += fmt.Sprintf("Set %s to %v\n", path, operations[path])
+		messages = append(messages, fmt.Sprintf("release(%s/%s): released by %s", service, stage, caller), "")
+		for _, cfg := range sortedOperations {
+			messages = append(messages, fmt.Sprintf("Set %s(%s) to %v", cfg, operations[cfg].keyPath, operations[cfg].value))
 		}
-		message := fmt.Sprintf("release(%s/%s): released by %s\n\n%s", service, stage, caller, body)
+		messages = append(messages, "")
 
-		err = repo.Commit(valuesFilePath, caller, message)
+		for _, m := range messages {
+			logrus.Println(m)
+		}
+
+		err = repo.Commit(valuesFilePath, caller, strings.Join(messages, "\n"))
 		if err != nil {
 			ch <- response{
 				Messages: append([]string{"this operation failed"}, messages...),
@@ -247,7 +273,6 @@ func deploy(ctx context.Context, clusterConfigs config.K8S, gitConfigs map[confi
 			_ = hardResetFn()
 			return
 		}
-		messages = strings.Split(message, "\n")
 	}
 	ch <- response{
 		Messages: messages,

command/deploy_test.go
@@ -46,7 +46,7 @@ func Test_deploy(t *testing.T) {
 				ctx:            ctx,
 				textParts:      []string{"tv", "prod", "yt-relay", "image:11", "pods:23"},
 			},
-			wantMessages: []string{"release(yt-relay/prod): released by @tester", "", "Set image.tag to 11", "Set replicacount to 23", ""},
+			wantMessages: []string{"release(yt-relay/prod): released by @tester", "", "Set image(image.tag) to 11", "Set pods(replicacount) to 23", ""},
 			wantErr:      false,
 		},
 	}
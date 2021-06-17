06f72696e05e8e9def7f5ffa029a52f7486c918b
Baron Chiu <21715194+bcgodev@users.noreply.github.com>
2021-06-18 07:19:35 +0800

    feat: add service support to list command to list history (#16)
    Add history so user can choose previous configuration to deploy.
    
    Refers: https://app.asana.com/0/1199959371256159/1195510318606286/f

--- command/list.go
+++ command/list.go
@@ -54,6 +54,30 @@ func List(ctx context.Context, clusterConfigs config.K8S, textParts []string) (m
 		for i, release := range releases {
 			messages[i] = fmt.Sprintf("%s: %s", release.Name, release.Status)
 		}
+	case 3:
+		kubeConfigPath, err := k8sop.SwitchKubeConfig(clusterConfigs, textParts[0], textParts[1])
+		if err != nil {
+			return nil, err
+		}
+		var namespace string
+		if textParts[2] == "cronjobs" {
+			namespace = "cron"
+		} else {
+			namespace = "default"
+		}
+		versions, err := k8sop.ListHelmReleaseVersion(kubeConfigPath, textParts[2], namespace, 11)
+		if err != nil {
+			return nil, err
+		}
+		for _, version := range versions {
+			messages = append(messages, fmt.Sprintf("%s(%d)", textParts[2], version.Version))
+			messages = append(messages, fmt.Sprintf("\t%s: %v", "ImageTag", version.ImageTag))
+			messages = append(messages, fmt.Sprintf("\t%s: %v", "Pods", version.ReplicaCount))
+			messages = append(messages, fmt.Sprintf("\t%s", "AutoScaling"))
+			messages = append(messages, fmt.Sprintf("\t\t%s: %v", "Enabled", version.AutoScaling.Enabled))
+			messages = append(messages, fmt.Sprintf("\t\t%s: %v", "Max Pods", version.AutoScaling.MaxReplicas))
+			messages = append(messages, fmt.Sprintf("\t\t%s: %v", "Min Pods", version.AutoScaling.MinReplicas))
+		}
 	default:
 		err = errors.Errorf("What is going on now?")
 	}
--- go.mod
+++ go.mod
@@ -9,7 +9,6 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/slack-go/slack v0.9.1
-	gopkg.in/yaml.v2 v2.4.0
 	helm.sh/helm/v3 v3.6.0
 	k8s.io/apimachinery v0.21.1
 	k8s.io/client-go v0.21.1
